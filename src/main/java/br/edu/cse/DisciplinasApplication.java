55442faf8864753c2106b7dc5f00bb26fd7b086c
eric lima <eric.george.lima@gmail.com>
2018-02-21 14:20:21 -0300

    inicial

---
+++ src/main/java/br/edu/cse/DisciplinasApplication.java
@@ -0,0 +1,12 @@
+package br.edu.cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinasApplication.class, args);
+	}
+}
